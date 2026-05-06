bde04ca3bbac56b9721e62625b33171fdc84f4e9
Chandan Rajpurohit <chandanrajpurohit476@gmail.com>
2026-05-07 00:52:40 +0530

    Day 2: spectra.py — gaussian_line() with broadcasting support, docstring complete

---
+++ spectra.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+wav = np.linspace(4990,5010,1000)
+wav0 = 5000.0
+wav0 = np.array([5000,5000.50,5010,5012])
+sigma = 1.0
+amp = 1.0
+
+
+def Gaussian_line(wav,wav0,sigma,amp):
+    wav0 = np.asarray(wav0)
+    wav  = np.asarray(wav)
+    if wav0.ndim == 0:
+        result =  amp*(np.exp(-(wav - wav0)**2) / (2 * sigma**2))
+        return result
+    
+    else:
+        result = amp*(np.exp(-(wav[:,np.newaxis] - wav0[np.newaxis,:])**2) / (2 * sigma**2))
+        return result
+    
+profile = Gaussian_line(wav,wav0,sigma,amp)
+print(profile)
