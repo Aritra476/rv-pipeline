57391b958c047367ec03a5a1b87a63cf1dd6e2c2
Chandan Rajpurohit <chandanrajpurohit476@gmail.com>
2026-05-10 09:20:43 +0530

    refine the spectra.py

--- spectra.py
+++ spectra.py
@@ -1,11 +1,5 @@
 import numpy as np
 
-wav = np.linspace(4990,5010,1000)
-wav0 = 5000.0
-wav0 = np.array([5000,5000.50,5010,5012])
-sigma = 1.0
-amp = 1.0
-
 
 def Gaussian_line(wav,wav0,sigma,amp):
     wav0 = np.asarray(wav0)
@@ -18,5 +12,14 @@ def Gaussian_line(wav,wav0,sigma,amp):
         result = amp * np.exp(-(wav[:, np.newaxis] - wav0[np.newaxis, :])**2 / (2 * sigma**2))
         return result
     
-profile = Gaussian_line(wav,wav0,sigma,amp)
-print(profile)
+
+if __name__ == "__main__":
+
+    wav = np.linspace(4990,5010,1000)
+    wav0 = 5000.0
+    wav0 = np.array([5000,5000.50,5010,5012])
+    sigma = 1.0
+    amp = 1.0
+
+    profile = Gaussian_line(wav,wav0,sigma,amp)
+    print(profile)
